1039a5482c803c254b61ea32dadb9faa6b45f0f5
Raman Singh <ramans@kater.com>
2019-07-10 21:12:42 -0700

    question 2 done but methods are not correct

--- src/Question2.java
+++ src/Question2.java
@@ -1,15 +1,32 @@
+/*
+Write a program that plays the Rock-Paper-Scissors-Lizard-Spock game. Refer to
+http://en.wikipedia.org/wiki/Rock-paper-scissors-lizard-Spock for more information.
+Normally, one player is a human and the other is the computer program. However, in this exercise,
+the program will generate two players who play against each other. The play continues until either
+of the computer-generated players wins four consecutive times.
+In this game, two random integers are generated in the range of [1 to 5], one per player. 1 refers to
+Rock, 2 refers to Paper, 3 refers to Scissors, 4 refers to Lizard, and 5 refers to Spock.
+For example, if the computer randomly generates integers 2 and 5 in the first iteration, 2 is for the
+first player and 5 is for the second player. Based on Rule 8 in the following 10 rules, Paper (2)
+disproves Spock (5), so Player 1 wins. Repeat it to generate one more pair and determine who wins
+that iteration. Continue the iterations until one player wins four consecutive times.
+Rule 1: Scissors cut paper
+Rule 2: Paper covers rock
+Rule 3: Rock crushes lizard
+Rule 4: Lizard poisons Spock
+Rule 5: Spock smashes (or melts) scissors
+Rule 6: Scissors decapitate lizard
+Rule 7: Lizard eats paper
+Rule 8: Paper disproves Spock
+Rule 9: Spock vaporizes rock
+Rule 10: Rock breaks scissors
+ */
+
 public class Question2 {
 
-
-
     public static void main(String[] args) {
-
-        for (int i=0; i<1000; i++) {
-            int a = (int)(Math.random() * 2) + 1;
-            System.out.println(a);
-        }
-
+        RockPaperScissorsLizardSpock rockPaperScissorsLizardSpock = new RockPaperScissorsLizardSpock();
+        rockPaperScissorsLizardSpock.play(1,2);
     }
-
 
 }
--- src/RockPaperScissorsLizardSpock.java
+++ src/RockPaperScissorsLizardSpock.java
@@ -1,3 +1,16 @@
+/*
+Rule 1: Scissors cut paper
+Rule 2: Paper covers rock
+Rule 3: Rock crushes lizard
+Rule 4: Lizard poisons Spock
+Rule 5: Spock smashes (or melts) scissors
+Rule 6: Scissors decapitate lizard
+Rule 7: Lizard eats paper
+Rule 8: Paper disproves Spock
+Rule 9: Spock vaporizes rock
+Rule 10: Rock breaks scissors
+ */
+
 public class RockPaperScissorsLizardSpock {
     private static int consecutiveWins;
     private static int lastWinner;
@@ -28,9 +41,117 @@ public class RockPaperScissorsLizardSpock {
     }
 
     public void play(int player1, int player2) {
-        int player1Hand = random();
-        int player2Hand = random();
-        
+
+        consecutiveWins = 0;
+        boolean gameOver = false;
+
+        do {
+            int lw = getLastWinner();
+            int player1Hand = random();
+            int player2Hand = random();
+            lastWinner = getWinner(player1Hand, player2Hand);
+            System.out.println("PLAYER" + lastWinner + " won this hand.");
+            if (lw == lastWinner) {
+                consecutiveWins++;
+            } else {
+                consecutiveWins = 1;
+            }
+            gameOver = (consecutiveWins == 4);
+
+        } while (!gameOver);
+
+        System.out.println("The winner is PLAYER" + lastWinner);
+
     }
 
+    private int getWinner(int player1Hand, int player2Hand) {
+
+        if (player1Hand == player2Hand) {
+            return getNextWinner();
+        }
+
+        System.out.println("Player 1: " + convert(player1Hand - 1));
+        System.out.println("Player 2: " + convert(player2Hand - 1));
+        if (player1Hand == ROCK) {
+            switch (player2Hand) {
+                case LIZARD:
+                case SCISSORS:
+                    return PLAYER1;
+                case PAPER:
+                case SPOCK:
+                    return PLAYER2;
+                    default:
+                        return getNextWinner();
+            }
+        }
+
+        if (player1Hand == PAPER) {
+            switch (player2Hand) {
+                case ROCK:
+                case SPOCK:
+                    return PLAYER1;
+                case LIZARD:
+                case SCISSORS:
+                    return PLAYER2;
+                    default:
+                        return getNextWinner();
+            }
+        }
+
+        if (player1Hand == SCISSORS) {
+            switch (player2Hand) {
+                case PAPER:
+                case LIZARD:
+                    return PLAYER1;
+                case SPOCK:
+                case ROCK:
+                    return PLAYER2;
+                default:
+                    return getNextWinner();
+            }
+        }
+
+        if (player1Hand == LIZARD) {
+            switch (player2Hand) {
+                case SPOCK:
+                case PAPER:
+                    return PLAYER1;
+                case ROCK:
+                case SCISSORS:
+                    return PLAYER2;
+                    default:
+                        return getNextWinner();
+            }
+        }
+
+        if (player1Hand == SPOCK) {
+            switch (player2Hand) {
+                case SCISSORS:
+                case ROCK:
+                    return PLAYER1;
+                case LIZARD:
+                case PAPER:
+                    return PLAYER2;
+                    default:
+                        return getNextWinner();
+            }
+        }
+
+        return getNextWinner();
+    }
+
+    private int getNextWinner() {
+        int p1Hand = random();
+        int p2Hand = random();
+        return getWinner(p1Hand, p2Hand);
+    }
+
+
+
 }
+
+
+/*
+
+Scissors > Paper > rock >lizard >spock >scissors >
+ */
