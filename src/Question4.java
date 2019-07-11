fa149333791973734f7f3606ef1b620d355b0ab0
Raman Singh <ramans@kater.com>
2019-07-11 09:15:22 -0700

    questin 4 almost done

---
+++ src/Question4.java
@@ -0,0 +1,36 @@
+/*
+ Craps is a dice game where two dice are rolled. Each die has six faces representing values
+1, 2, 3, 4, 5, or 6.
+I. If the sum is 2, 3, or 12 (called craps), you lose;
+II. If the sum is 7 or 11 (called natural), you win;
+III. If the sum is any other value (4, 5, 6, 8, 9, or 10), a value point is established, and you continue
+to roll until you either roll a sum of the value point or a 7. If the sum of the new roll is equal to
+the value point, then you win; if the sum of the new roll is equal to 7, then you lose.
+ Remember, in option (III), you continue to roll until you get a 7 or the value point.
+Sample runs:
+• You rolled 5 + 6 = 11; you win
+• You rolled 1 + 2 = 3; you lose
+• You rolled 2 + 2 = 4; you establish the value point 4;
+– Roll again 2 + 3 = 5; roll
+– Roll again 2 + 1 = 3; roll
+– Roll again 2 + 2 = 4; you win
+• You rolled 2 + 6 = 8; you establish the value point 8;
+– Roll again 4 + 4 = 8; you win
+• You rolled 3 + 2 = 5; you establish the value point 5;
+– Roll again 1 + 1 = 2; roll
+– Roll again 2 + 2 = 4; roll
+Computer Science 268: Introduction to Programming in Java Page 5 of 12
+– Roll again 1 + 1 = 2; roll
+– Roll again 3 + 4 = 7; you lose
+Develop a program that plays craps with a player three times. At the end, the program prints the
+number of times the player won and the number of times the player lost.
+ */
+
+public class Question4 {
+
+    public static void main(String[] args) {
+        RandomSumGame randomSumGame = new RandomSumGame();
+        boolean a = randomSumGame.play();
+    }
+
+}
---
+++ src/RandomSumGame.java
@@ -0,0 +1,69 @@
+public class RandomSumGame {
+
+    boolean start;
+    int d1, d2;
+    int sum;
+    int valuePoint;
+    String status;
+
+    public void play(int d1, int d2) {
+
+    }
+
+    public RandomSumGame() {
+        status = "";
+        valuePoint = 0;
+    }
+
+    public boolean play() {
+        rollDice();
+        return won();
+
+    }
+
+
+    private boolean won() {
+
+        switch (sum) {
+            case 2:
+            case 3:
+            case 12:
+                System.out.println("You rolled " + d1 + " + " + d2 + " = " + sum + "; You lose");
+                return false;
+            case 7:
+            case 11:
+                System.out.println("You rolled " + d1 + " + " + d2 + " = " + sum + "; You win");
+                return true;
+            default:
+                if (valuePoint == 0) {
+                    valuePoint = sum;
+
+                    System.out.println("You rolled " + d1 + " + " + d2 + " = " + sum + "; You established a value point " + valuePoint + ";");
+                    do {
+                        rollDice();
+                        if (sum == valuePoint) {
+                            System.out.println("- Roll again " + d1 + " + " + d2 + " = " + sum + "; you win");
+                            return true;
+                        }
+                        if (sum == 7) {
+                            System.out.println("- Roll again " + d1 + " + " + d2 + " = " + sum + "; you lose");
+                            return false;
+                        }
+
+                        System.out.println("- Roll again " +  d1 + " + " + d2 + " = " + sum + "; roll");
+
+                    } while (true);
+                }
+
+                return (sum == valuePoint);
+            }
+    }
+
+
+    public void rollDice() {
+        d1 = (int) (Math.random() * 6) + 1;
+        d2 = (int) (Math.random() * 6) + 1;
+        sum = d1 + d2;
+    }
+
+}
